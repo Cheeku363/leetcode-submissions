8adad6d695fcf45ab6d53bfde96ccf27187de721
Chirag Jhariya <chiragjhariya7@gmail.com>
2022-02-14 17:37:53 +0530

    Arrays

---
+++ Arrays/Easy/Running_Sum_1DArr.cpp
@@ -0,0 +1,11 @@
+// Pretty Easy No?
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> v;
+        int sum = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            v.push_back(sum);
+        }
+        return v;
+    }
