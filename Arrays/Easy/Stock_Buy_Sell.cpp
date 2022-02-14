9f42ffdcd12987cd31d86b710834313b45729353
Chirag Jhariya <chiragjhariya7@gmail.com>
2022-02-14 17:05:21 +0530

    arrays

--- Arrays/Easy/Stock_Buy_Sell.cpp
+++ Arrays/Easy/Stock_Buy_Sell.cpp
@@ -1,9 +1,9 @@
  int maxProfit(vector<int>& prices) {
         int maxPro = 0;
-        int minPrice = INT_MAX;
+        int minPrice = INT_MAX; // set Maximum Integer value
         for(int i = 0; i < prices.size(); ++i){
             minPrice = min(minPrice, prices[i]);
-            maxPro = max(maxPro, prices[i] - minPrice);
+            maxPro = max(maxPro, prices[i] - minPrice); // calculate max profit so far.
         }
         return maxPro;
     }
--- Arrays/Easy/merge_Sorted_arrays.cpp
+++ Arrays/Easy/merge_Sorted_arrays.cpp
@@ -1,9 +1,13 @@
  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int a = m-1, b = n-1, i = m+n-1;
+        int a = m-1, b = n-1;
+        int i = m+n-1;  // calculate the index of the last element of the merged array
+        
+        // go from the back by nums1 and nums2 and compare and put to the nums1 element which is larger
         while(a >= 0 && b >= 0){
             if(nums1[a] > nums2[b]) nums1[i--] = nums1[a--];
             else nums1[i--] = nums2[b--];
         }
+        // if nums2 is longer than nums1 just copy the rest of nums2 to nums1 location, otherwise no need to do anything
         while(b >= 0){
             nums1[i--] = nums2[b--];
         }
